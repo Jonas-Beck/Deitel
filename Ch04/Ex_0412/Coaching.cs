3a4cf10e112ee12502a146b35ad27e1fa30c3899
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-09-20 11:43:19 +0200

    Ex_0412

---
+++ Ch04/Ex_0412/Coaching.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex_0412
+{
+    internal class Coaching
+    {
+        public string CoachingType { get; set; }
+        public int NumberOfPlayers { get; set; }
+        public string ClassTimings { get; set; }
+        public decimal Charges { get; set; }
+
+        public Coaching(string coachingType, int numberOfPlayers, string classTiming, decimal charges)
+        {
+            CoachingType = coachingType;
+            NumberOfPlayers = numberOfPlayers;
+            ClassTimings = classTiming;
+            Charges = charges;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Coaching type: {CoachingType}");
+            Console.WriteLine($"Number of players: {NumberOfPlayers}");
+            Console.WriteLine($"Class Timings: {ClassTimings}");
+            Console.WriteLine($"Charge for Coaching {Charges:C}");
+            Console.WriteLine();
+        }
+    }
+}
--- Ch04/Ex_0412/Program_0412.cs
+++ Ch04/Ex_0412/Program_0412.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Coaching Coach = new Coaching("Soccer", 10, "Class Timing", 500);
+
+            Coach.DisplayDetails();
+
+            Coach.NumberOfPlayers = 5;
+            Coach.Charges = 250;
+
+            Coach.DisplayDetails();
         }
     }
 }
