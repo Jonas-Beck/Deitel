31d207120789a6def946ad0e70cd027c3a562037
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-09-20 13:38:43 +0200

    Ex 0415

---
+++ Ch04/Ex_0415/HealthProfile.cs
@@ -0,0 +1,52 @@
+﻿namespace Ex_0415
+{
+    internal class HealthProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int MonthOfBirth { get; set; }
+        public int DayOfBirth { get; set; }
+        public int YearOfBirth { get; set; }
+        // Height in inches
+        public int Height { get; set; }
+        // Weight in Pounds
+        public int Weight { get; set; }
+
+        public HealthProfile(string firstName, string lastName, string gender, int monthOfBirth, int dayOfBirth, int yearOfBirth, int height, int weight)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            MonthOfBirth = monthOfBirth;
+            DayOfBirth = dayOfBirth;
+            YearOfBirth = yearOfBirth;
+            Height = height;
+            Weight = weight;
+        }
+
+        public int AgeInYears()
+        {
+            DateTime BirthDate = new DateTime(YearOfBirth, MonthOfBirth, DayOfBirth);
+            int age = DateTime.Now.Year - BirthDate.Year;
+            if (DateTime.Now.Year < BirthDate.DayOfYear)
+                age = age - 1;
+
+            return age;
+        }
+        public int MaxHeartRate()
+        {
+            return 1;
+        }
+        public int TargetHeartRate()
+        {
+            return 1;
+        }
+        public double BMI()
+        {
+            return 1;
+
+        }
+
+    }
+}
---
+++ Ch04/Ex_0415/Program_0415.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex_0415
+{
+    internal class Program_0415
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your firstname: ");
+            string firstName = Console.ReadLine();
+            Console.Write("What is your lastname: ");
+            string lastName = Console.ReadLine();
+            Console.Write("What is your gender: ");
+            string gender = Console.ReadLine();
+
+            Console.Write("What month are you born(Number format): ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What day are you born(Number format): ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What year are you born: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Height in inches: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Weight in Pounds: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            HealthProfile profile = new HealthProfile(firstName, lastName, gender, month, day, year, height, weight);
+
+            Console.WriteLine(profile.AgeInYears());
+
+
+        }
+    }
+}
