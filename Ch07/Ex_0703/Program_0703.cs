26d5bfe433cf243caa97d02a79bf0549be3f4960
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-09-21 10:53:31 +0200

    Ex_0703

---
+++ Ch07/Ex_0703/Program_0703.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex_0703
+{
+    internal class Program_0703
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Abs(23.7) is {Math.Abs(23.7)}");
+            Console.WriteLine($"Abs(0.0) is {Math.Abs(0.0)}");
+            Console.WriteLine($"Abs(-23.7) is {Math.Abs(-23.7)}");
+            Console.WriteLine($"Ceiling(9.2) is {Math.Ceiling(9.2)}");
+            Console.WriteLine($"Ceiling(-9.8) is {Math.Ceiling(-9.8)}");
+            Console.WriteLine($"Floor(9.2) is {Math.Floor(9.2)}");
+            Console.WriteLine($"Floor(-9.8) is {Math.Floor(-9.8)}");
+            Console.WriteLine($"Cos(0.0) is {Math.Cos(0.0)}");
+            Console.WriteLine($"Sin(0.0) is {Math.Sin(0.0)}");
+            Console.WriteLine($"Tan(0.0) is {Math.Tan(0.0)}");
+            Console.WriteLine($"Exp(1.0) is {Math.Exp(1.0)}");
+            Console.WriteLine($"Exp(2.0) is {Math.Exp(2.0)}");
+            Console.WriteLine($"Log(Math.E) is {Math.Log(Math.E)}");
+            Console.WriteLine($"Log(Math.E * Math.E) is {Math.Log(Math.E * Math.E)}");
+            Console.WriteLine($"Max(2.3, 12.7) is {Math.Max(2.3, 12.7)}");
+            Console.WriteLine($"Max(-2.3, -12.7) is {Math.Max(-2.3, -12.7)}");
+            Console.WriteLine($"Min(2.3, 12.7) is {Math.Min(2.3, 12.7)}");
+            Console.WriteLine($"Min(-2.3, -12.7) is {Math.Min(-2.3, -12.7)}");
+            Console.WriteLine($"Pow(2.0, 7.0) is {Math.Pow(2.0, 7.0)}");
+            Console.WriteLine($"Pow(9.0, 0.5) is {Math.Pow(9.0, 0.5)}");
+            Console.WriteLine($"Sqrt(900.0) is {Math.Sqrt(900.0)}");
+        }
+    }
+}
