5852878254c731f9ee08cda0018bfbeb503021ea
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-09-21 11:08:46 +0200

    Ex_0706

---
+++ Ch07/Ex_0706/Program_0706.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex_0706
+{
+    internal class Program_0706
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write the radius of sphere: ");
+            double answer = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"Volume of the sphere: {SphereVolume(answer)}");
+        }
+
+        static double SphereVolume(double radius) => (4.0 / 0.3) * Math.PI * Math.Pow(radius, 3);
+    }
+}
