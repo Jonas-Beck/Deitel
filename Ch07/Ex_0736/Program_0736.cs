21dc207127485323677fa76673b527565103b084
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-09-21 13:13:21 +0200

    Ex_0736

---
+++ Ch07/Ex_0736/Program_0736.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex_0736
+{
+    internal class Program_0736
+    {
+        static void Main(string[] args)
+        {
+            Tower(2, 1, 3, 2);
+        }
+
+        static void Tower(int NumberOfDisks, int StartingPeg, int TargetPeg, int TemporaryPeg)
+        {
+            if (NumberOfDisks == 1)
+            {
+                Console.WriteLine($"{StartingPeg} > {TargetPeg}");
+            }
+            else
+            {
+                Tower(NumberOfDisks - 1, StartingPeg, TemporaryPeg, TargetPeg);
+                Console.WriteLine($"{StartingPeg} > {TargetPeg}");
+                Tower(NumberOfDisks - 1, TemporaryPeg, TargetPeg, StartingPeg);
+            }
+        }
+    }
+}
