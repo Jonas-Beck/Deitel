d254e2e1304b66dc237a04ea48fdd7202770c04a
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-10-04 09:44:41 +0200

    Ex 1605

---
+++ Ch16/Ex_1605/Program_1605.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex_1605
+{
+    internal class Program_1605
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write English Word Sentence:");
+            string answer = Console.ReadLine();
+            string PigString = "";
+
+            foreach (var word in answer.Split(" "))
+            {
+                PigString += GetPigLatin(word) + " ";
+            }
+            Console.WriteLine("Translated to Pig Lating:");
+            Console.WriteLine(PigString);
+        }
+
+        public static string GetPigLatin(string EnglishString)
+        {
+            if (EnglishString.Length == 0) { return EnglishString; }
+
+            string PigString = EnglishString.Substring(1);
+            PigString += EnglishString[0] + "ay";
+            return PigString;
+        }
+    }
+}
