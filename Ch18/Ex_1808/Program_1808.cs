65d0914f106e596c1d143145417b3545ca3b6bd0
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-10-05 11:09:18 +0200

    Ex 1808

---
+++ Ch18/Ex_1808/Program_1808.cs
@@ -0,0 +1,45 @@
+﻿namespace Ex_1808
+{
+    internal class Program_1808
+    {
+        static void Main(string[] args)
+        {
+            var generator = new Random();
+            var data = new int[10];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = generator.Next(10, 100);
+            }
+
+            Console.WriteLine(String.Join(" ", data));
+            Console.Write("Write number to search for:");
+            int key = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Result: {RecursiveLinearSearch(data, key, 0)}");
+
+        }
+
+        public static int RecursiveLinearSearch(int[] values, int searchKey, int startingIndex)
+        {
+            if (startingIndex == values.Length)
+            {
+                return -1;
+            }
+            return values[startingIndex] == searchKey ? startingIndex : RecursiveLinearSearch(values, searchKey, startingIndex + 1);
+        }
+
+        /* Fig 18.2
+        public static int LinearSearch(int[] values, int searchKey)
+        {
+            for (int index = 0; index < values.Length; ++index)
+            {
+                if (values[index] == searchKey)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+        */
+    }
+}
