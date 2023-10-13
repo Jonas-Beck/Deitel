6aac540c17ba126e0579a7c77a23b353f824d6d3
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-10-13 09:47:16 +0200

    Ex 1904

---
+++ Ch19/Ex_1904/Classes/StackInheritance.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex_1904.Classes
+{
+    internal class StackInheritance : List
+    {
+        public StackInheritance() : base("Stack") { }
+
+        public void Push(object dataValue)
+        {
+            InsertAtBack(dataValue);
+        }
+
+        public void Pop()
+        {
+            RemoveFromBack();
+        }
+    }
+}
---
+++ Ch19/Ex_1904/Program_1904.cs
@@ -0,0 +1,30 @@
+﻿using Ex_1904.Classes;
+
+namespace Ex_1904
+{
+    internal class Program_1904
+    {
+        static void Main(string[] args)
+        {
+
+            StackInheritance stack = new StackInheritance();
+            stack.Display();
+            Console.WriteLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Stack push({i})");
+                stack.Push(i);
+                stack.Display();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Stack Pop()");
+                stack.Pop();
+                stack.Display();
+            }
+
+        }
+    }
+}
