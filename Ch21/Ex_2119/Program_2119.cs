8d66814e1871ea9af27238d9d4f3150a381ab5e5
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-10-25 10:49:49 +0200

    Ex 2119

---
+++ Ch21/Ex_2119/Program_2119.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex_2119
+{
+    internal class Program_2119
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            IEnumerable<IGrouping<int, int>> values1 = Enumerable.Range(1, 60000000)
+                                                                 .Select(x => random.Next(1, 7))
+                                                                 .GroupBy(value => value)
+                                                                 .OrderBy(value => value.Key);
+
+            Console.WriteLine($"{"Face"}{"Frequency",10}");
+            foreach (var value in values1)
+            {
+                Console.WriteLine($"{value.Key,4}{value.Count(),10}");
+
+            }
+
+        }
+    }
+}
